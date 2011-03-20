6c5f7cb26c380962eb79978aa99e087cc7b6714b
David Wilson <dw@botanicus.net>
2011-03-20 13:57:44 +0000

    Import basic smoke test script.

---
+++ smoke_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""Quick and dirty smoke test that looks up IPs using the Python library and
+command line tool, printing any mismatches."""
+
+import os
+import random
+import re
+import sys
+
+from socket import inet_aton
+from socket import inet_ntoa
+
+import pygeoip
+
+
+MATCH_PATTERN = re.compile('GeoIP Country Edition: ([A-Z0-9][0-9A-Z]),.*')
+TRIES = 20000
+
+
+def make_ip():
+    """Return a random IP address as a string."""
+    return inet_ntoa(inet_aton(str(random.randint(0, 0xffffffff))))
+
+def clookup(ip):
+    """Get the country code for an IP address using the 'geoiplookup'
+    utility."""
+    with os.popen('geoiplookup ' + ip) as fp:
+        match = MATCH_PATTERN.match(fp.read())
+    if match:
+        return match.group(1)
+
+def main():
+    db = pygeoip.Database('GeoIP.dat')
+
+    for ip in (make_ip() for x in xrange(TRIES)):
+        cc1 = clookup(ip)
+        cc2 = db.lookup(ip).country
+
+        if cc1 != cc2:
+            print
+            print 'mismatch', ip, cc1, cc2
+
+if __name__ == '__main__':
+    main()
