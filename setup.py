6acd18098b338836a6036b0e638b62513a373437
David Wilson <dw@botanicus.net>
2016-02-12 05:59:05 +0000

    issue #1: update setup.py for PyPI hosting.

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-'''setuptools setup.py for CBMS.'''
-
 from distutils.core import setup
 
 setup(
     name='Pure Python GeoIP Library',
-    version='0.1',
+    version='0.2',
     description='''
-        Library to perform lookups in Maxmind GeoIP, designed specifically for use with Google App Engine.
+        Library to perform lookups in Maxmind GeoIP, designed specifically for
+        use with Google App Engine.
     ''',
-    author='David Wilson',
-    author_email='dw+geoip@botanicus.net',
-    url='http://code.google.com/p/python-geoip/',
-    download_url='http://python-geoip.googlecode.com/hg/pygeoip.py',
+    url='https://github.com/dw/python-geoip',
     py_modules=['pygeoip']
 )
